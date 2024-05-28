218229cb3fba6b229984aac6d940a2d3bc1cb99f
Jason Preszler <jpreszler@gmail.com>
2024-05-27 17:59:59 -0700

    added neovim from mac

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,26 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {"lua_ls", "ruff", "yamlls", "cypher_ls"}
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            lspconfig.ruff.setup({})
+            lspconfig.yamlls.setup({})
+            lspconfig.cypher_ls.setup({})
+        end
+    }
+}
