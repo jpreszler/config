37a2120cdd3d2a3aa07e4580947ceb4a9e974613
Jason Preszler <jpreszler@gmail.com>
2024-06-19 14:31:39 -0700

    add precog and telekasten

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -2,12 +2,12 @@ return {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim", "nvim-lua/telescope.nvim"},
-    config = function() 
+    config = function()
         local harpoon = require("harpoon")
         harpoon:setup()
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
+        vim.keymap.set("n", "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<leader>hc", function() harpoon:list():clear() end)
         vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
         vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
         vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
