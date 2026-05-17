96b9f86ebfb4d4f7944e726715c6659bbdc5c7fb
Jason Preszler <jpreszler@gmail.com>
2026-05-16 17:22:05 -0700

    treesitter updates

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,13 +2,18 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     lazy=false,
+    branch="main",
     build = ":TSUpdate",
-    config = function()
-        local config = require("nvim-treesitter")
-        config.setup({
-            ensure_installed = {"lua", "python", "markdown", "markdown_inline", "go"},
-            highlight = { enable = true},
-            indent = { enable = true},
+    init = function()
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function()
+                pcall(vim.treesitter.start)
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
         })
-    end
+        local ensure_installed = {
+            "lua", "python", "markdown", "go", "markdown_inline"
+        }
+        require('nvim-treesitter').install(ensure_installed)
+    end,
 }
